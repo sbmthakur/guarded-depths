818ab05394d48fa05c1171c3ade85f5b5e5208d3
shubham thakur <sbmthakur@gmail.com>
2018-02-05 00:59:52 +0530

    Use Mongodb for datastorage

--- index.js
+++ index.js
@@ -10,6 +10,31 @@ const shortid = require("shortid");
 const express = require("express");
 const path = require("path");
 const app = express();
+
+const MongoClient = require("mongodb").MongoClient;
+/* 
+ * Connection URL
+ * 'mlab' provides free Mongo instance for quick
+ * prototyping. A big thanks to them!
+ */
+const url = "mongodb://root:root@ds123258.mlab.com:23258/guarded-depths";
+ 
+// Database Name
+const dbName = "guarded-depths";
+ 
+// Use connect method to connect to the server
+let collection; 
+
+MongoClient.connect(url, function(err, client) {
+  let db = client.db(dbName);
+  collection = db.collection("test");
+  /*
+   * The following index will be used
+   * for searching
+   */
+  collection.createIndex({ "name": "text" });
+});
+
 app.use(express.json());
 
 /*
@@ -21,12 +46,10 @@ app.use(express.static(path.resolve(__dirname, "ui")));
  * Middleware for all API requests
  * Handle CORS
  */
-app.use('/skills', function (req, res, next) {
+app.use("/skills", function (req, res, next) {
   res.setHeader("Access-Control-Allow-Origin", "*");
   next();
 });
-
-const data = {};
 
 /*
  * API: Fetch all skills
@@ -37,17 +60,14 @@ const data = {};
  */
 app.get("/skills/get", (req, res) => {
 
-  const skillIds = Object.keys(data);
-  let skillList = []
-  for(let skillId of skillIds) {
+  collection.find({}).toArray((err, docs) => {
   
-    skillList.push({
-      "id": skillId,
-      "name": data[skillId].name,
-      "status": data[skillId].status
+    docs.map(doc => {
+      doc.id = doc._id;
+      delete doc._id;
     });
-  }
-  res.send(skillList);
+    res.send(docs);    
+  });
 });
 
 /*
@@ -64,9 +84,53 @@ app.post("/skills/add", (req, res) => {
     status: req.body.status
   };
   const id = shortid.generate();
-  data[id] = newSkill;
-  res.send({"id": id});
+  newSkill._id = id;    
+  /*
+   * Add new skill to our Mongo database
+   */
+  collection.insertOne(newSkill, (err, result) => {
+    if(err) 
+      res.status(500).send("Internal Error");
+    else {
+      res.send({"id": id});
+    }
+  });
 });
+
+/*
+ * editProperty: This is used to set the status or name 
+ * property of a skill. This is called by Edit API or the 
+ * Change status API
+ */
+function editProperty({id, property, value, responseObject}) {
+
+  const filter = {
+    "_id": id 
+  };
+  const modifiedData = {
+    "$set": {}
+  };
+
+  modifiedData["$set"][property] = value;
+
+  collection.updateOne(filter, modifiedData, (err, result) => {
+  
+    if(err) {
+      responseObject.status(500).send("Internal Error");
+    }
+    else {
+      if(result.result.n === 0) {
+        responseObject.status(401).send("Unauthorized");
+      }
+      else if (result.result.n === 1) {
+        if(value === null)
+          responseObject.send("null");
+        else
+          responseObject.send(value);
+      }
+    }
+  });
+}
 
 /*
  * API: Edit skill name
@@ -78,14 +142,16 @@ app.post("/skills/add", (req, res) => {
 app.post("/skills/edit", (req, res) => {
  
   const id = req.body.id;
-
-  if(data.hasOwnProperty(id)) {
-    data[id].name = req.body.name;
-    res.send("Success");
+  let property, value;
+  if(req.body.hasOwnProperty("name")) {
+    property = "name"; 
   }
   else {
-    res.status(401).send("Unauthorized"); 
+    property = "status"; 
   }
+  value = req.body[property];
+
+  editProperty({ id: id, property: property, value: value, responseObject: res });
 });
 
 /*
@@ -95,24 +161,42 @@ app.post("/skills/edit", (req, res) => {
  *  -> status
  */
 app.post("/skills/status", (req, res) => {
+
   const id = req.body.id;
-  if(data.hasOwnProperty(id)) {
-    
-    if(data[id].status === req.body.status) {
-    
-      data[id].status = null;
-      console.log('lllol')
-      res.send("null");
-    }
-    else {
-    
-      data[id].status = req.body.status;
-      res.send(data[id].status);
-    }
+
+  let property, value;
+  if(req.body.hasOwnProperty("name")) {
+    property = "name"; 
   }
   else {
-    res.status(401).send("Unauthorized"); 
+    property = "status"; 
   }
+  value = req.body[property];
+
+  const findFilter = {
+    "_id": id
+  };
+  /*
+   * We need to fetch the existing status
+   * of the skill. This will be used to 
+   * decide whether skill should be set to 
+   * null or not.
+   */
+  collection.findOne(findFilter,(err, doc) => {
+    if(err) {
+      return res.status(500).send("Internal Error");  
+    }
+
+    if(!doc) {
+      return res.status(401).send("Unauthorized");   
+    }
+
+    if(doc.status === value) {
+      value = null;
+    }
+
+    editProperty({ id: id, property: property, value: value, responseObject: res });
+  });
 });
 
 /*
@@ -122,28 +206,25 @@ app.post("/skills/status", (req, res) => {
  *  -> status
  */
 app.get("/skills/search", (req, res) => {
- 
-  let found = false;
-  let id;
-  let searchData = [];
-  for(id in data) {
-    
-    if(data[id].name === req.query.term) {
-      found = true;
-      searchData.push({
-        "id": id,
-        "name": data[id].name,
-        "status": data[id].status
-      });
-    }
-  }
 
-  if(found) {
-    res.send(searchData); 
-  }
-  else {
-    res.status(204).send("Not Found");   
-  }
+  const findFilter = { $text: { $search: req.query.term}};
+  collection.find(findFilter).toArray( (err, docs) => {
+   
+    if(err) {
+      return res.status(500).send("Internal Error"); 
+    }
+    if(docs.length === 0) {
+      res.status(204).send("Not Found");    
+    }
+    else {
+    
+      docs.map(doc => {
+        doc.id = doc._id;
+        delete doc._id;
+      });
+      res.send(docs);    
+    }
+  });
 });
  
 app.listen(process.env.PORT || 3000);
